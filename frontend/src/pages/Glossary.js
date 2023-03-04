de0e846115b86a9803790805543abfc131eaef14
Eugene Lok <43281502+eugene-lok@users.noreply.github.com>
2023-03-04 14:56:35 -0700

    Fix css for pdf wrapper for glossary, add title

--- frontend/src/pages/Glossary.js
+++ frontend/src/pages/Glossary.js
@@ -7,7 +7,10 @@ function Glossary() {
   return (
     <div>
       <Navbar/>
+      <div className = "glossaryWrapper">
+      <h2> Glossary </h2>
       <embed src={glossary} type="application/pdf" width="100%" height="500px" />
+      </div>
     </div>
   );
 }
